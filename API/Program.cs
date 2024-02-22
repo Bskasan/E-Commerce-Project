64a7718e276e68e16684a250217c2b083cf79944
Bskasan <b.kasan@yandex.com>
2024-02-22 23:58:39 +0200

    up -

--- API/Program.cs
+++ API/Program.cs
@@ -31,4 +31,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Create a scoper and store it in a variable
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (System.Exception ex)
+{
+    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+}
+
 app.Run();
